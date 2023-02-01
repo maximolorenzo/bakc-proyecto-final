0f217a4ef36469f13cf4cb098b79127fb3814dfb
maximolorenzoo <maximolorenzoo@gmail.com>
2023-01-31 21:04:06 -0300

    proyecto1

--- src/routes/views router/product.views.router.js
+++ src/routes/views router/product.views.router.js
@@ -8,16 +8,27 @@ router.get("/", async (req, res) => {
   const limit = req.query?.limit || 10;
   const page = req.query?.page || 1;
   const filter = req.query?.filter || "";
-
+  const sortQuery = req.query?.sort || "";
+  const sortQueryOrder = req.query?.sortorder || "desc";
   const search = {};
   if (filter) {
     search.title = filter;
   }
 
-  const options = { limit, page, lean: true };
+  const sort = {};
+  if (sortQuery) {
+    sort[sortQuery] = sortQueryOrder;
+  }
+
+  const options = {
+    limit,
+    page,
+    sort,
+    lean: true,
+  };
 
   const data = await productsModel.paginate(search, options);
-  //console.log(JSON.stringify(data, null, 2, "\t"));
+  console.log(JSON.stringify(data, null, 2, "\t"));
 
   res.render("products", data);
 });
